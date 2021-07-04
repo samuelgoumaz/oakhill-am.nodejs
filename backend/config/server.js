ca43ea42cb9cbef18a9e88b66fcc5dc88fa5ba67
samuelgoumaz <>
2021-07-04 19:29:02 +0200

    update with ip public

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '185.34.102.27'),
+  port: env.int('PORT', 8080),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'dbf6c53568c1d91f1a4bdbee832f324f'),
